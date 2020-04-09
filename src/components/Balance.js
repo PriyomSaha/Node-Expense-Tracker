d837e0c5790e87329f78977410a4f97aaedfa25e
Priyom Saha <priyom1499@gmail.com>
2020-04-09 08:37:39 +0530

    09 april,2020

---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <div className="container">
+            <h3>Your Balance</h3>
+            <h2>Rs : 00.00</h2>
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h1>
+          <u>Expense Tracker</u> 
+        </h1>
+    )
+}
