0be3c726021decb72b897a012d0c969369e45c64
Priyom Saha <priyom1499@gmail.com>
2020-04-09 22:19:29 +0530

    4th APRIL,2020-10:20pm

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
     return (
         <div className="container">
             <h3>Your Balance</h3>
---
+++ src/context/GlobalState.js
@@ -0,0 +1,25 @@
+import React , { createContext , useReducer} from 'react';
+import AppReducer from './AppReducer'
+
+//Initial Satate
+const initialState = {
+    transactions:[
+        {id:1,text:'Flower',amount:-10},
+        {id:2,text:'Salary',amount:+2500},
+        {id:3,text:'Electricity',amount:-400},
+        {id:4,text:'Fees',amount:+200},
+    ]
+}
+
+//Create Context
+export const GlobalContext = createContext(initialState);
+
+//Provider Component
+export const GlobalProvider = ({ children }) => {
+    const [state,dispatch] = useReducer(AppReducer,initialState);
+    return(<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+}
